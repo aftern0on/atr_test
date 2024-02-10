7e757469db832a47670914d60dc5fc3cc28ad059
aftern0on <faa31.31.31.31@mail.ru>
2024-02-11 02:03:44 +0300

    Замена logging-info на debug

--- backend/settings.py
+++ backend/settings.py
@@ -47,7 +47,7 @@ LOGGING = {
     'disable_existing_loggers': False,
     'handlers': {
         'console': {
-            'level': 'DEBUG',
+            'level': 'INFO',
             'class': 'logging.StreamHandler',
         },
     },
